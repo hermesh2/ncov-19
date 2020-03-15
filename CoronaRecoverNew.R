916bcb29aa4794d2519b97da366a461e2aeb6caa
hermesh2 <javier.villacampa.gonzalez@gmail.com>
2020-03-15 04:41:24 +0100

    algunas mejoras para publicar el articulo

--- CoronaRecoverNew.R
+++ CoronaRecoverNew.R
@@ -1,12 +1,16 @@
 rm(list =ls());gc()
+
 library(data.table)
 library(dplyr)
 library(zoo)
 library(forecast)
 library(lubridate)
+library(dygraphs)
 
+fecIni <- 20200101
+n <- 3
 setwd("C:/Users/hermesh/Dropbox/Investigaciones/Coronavirus/")
-
+# setwd("C:/Users/herme//Dropbox/Investigaciones/Coronavirus/")
 funcMelt <- 
   function(dataFun, varTxt = "txt" ){
     dataFun <- 
@@ -56,6 +60,10 @@ new5 <- dcast(data = new4[ , .(var = sum(newCases, na.rm = TRUE)), by = .(Coun, 
 dea5 <- dcast(data = dea4[ , .(var = sum(newCases, na.rm = TRUE)), by = .(Coun, Fecha) ], formula = Fecha ~  Coun, value.var = "var", fill =  0)
 rec5 <- dcast(data = rec4[ , .(var = sum(newCases, na.rm = TRUE)), by = .(Coun, Fecha) ], formula = Fecha ~  Coun, value.var = "var", fill =  0)
 
+# Soluciono el problema del decalaje
+new5$Fecha <- new5$Fecha + days(1)
+dea5$Fecha <- dea5$Fecha + days(1)
+rec5$Fecha <- rec5$Fecha + days(1)
 datos <- new5 %>%  select(Fecha, china, `korea,_south`,italy, spain)  %>%  data.frame()
 xts::xts(x = datos[ , names(datos)[names(datos) != "Fecha"]], order.by = datos$Fecha  ) %>%  dygraphs::dygraph()
 
@@ -68,14 +76,14 @@ DTnames <- names(datos)[names(datos) !=  "Fecha"]
 datos <- data.table(datos)
 datos[ , (DTnames) := lapply( .SD, foo ), .SDcol = DTnames ]
 datos  
-datosNew <- data.frame(datos[ Fecha > ymd(20200215) & Fecha != Sys.Date() - days(1)])
+datosNew <- data.frame(datos[ Fecha > ymd(fecIni) & Fecha <= Sys.Date() - days(n)])
 xts::xts(x = datosNew[ , names(datosNew)[names(datosNew) != "Fecha"]], order.by = datosNew$Fecha  ) %>%  dygraphs::dygraph()
 
 datos <- rec5 %>%  select(Fecha, china, `korea,_south`,italy, spain)  %>%  data.frame()
 datos <- data.table(datos)
 datos[ , (DTnames) := lapply( .SD, foo ), .SDcol = DTnames ]
 datos  
-datosRecover <- data.frame(datos[ Fecha > ymd(20200215) & Fecha != Sys.Date() - days(1)])
+datosRecover <- data.frame(datos[ Fecha > ymd(fecIni) & Fecha <= Sys.Date() - days(n)])
 xts::xts(x = datosRecover[ , names(datosRecover)[names(datosRecover) != "Fecha"]], order.by = datosRecover$Fecha  ) %>%  dygraphs::dygraph()
 
 datosAll <- merge(x = datosNew, y = datosRecover, by = "Fecha", all = TRUE)
@@ -91,6 +99,14 @@ xts::xts(x = datosAll[ , names(datosAll)[grep( pattern = "spain", x = names(dato
 xts::xts(x = datosAll[ , names(datosAll)[grep( pattern = "italy", x = names(datosAll) )   ]]
          , order.by = datosAll$Fecha  ) %>%  dygraphs::dygraph()
 
+datos <- dea5 %>%  select(Fecha, china, `korea,_south`,italy, spain)  %>%  data.frame()
+datos <- data.table(datos)
+datos[ , (DTnames) := lapply( .SD, foo ), .SDcol = DTnames ]
+datos  
+datosDeath <- data.frame(datos[ Fecha > ymd(fecIni) & Fecha <= Sys.Date() - days(n)])
+xts::xts(x = datosDeath[ , names(datosDeath)[names(datosDeath) != "Fecha"]], order.by = datosDeath$Fecha  ) %>%  dygraphs::dygraph()
+names(datosDeath)[-1] <- paste0(names(datosDeath)[-1] , "_death")
+datosAll <- merge(x = datosAll, y = datosDeath, by = "Fecha", all = TRUE)
 
 # hay algo raro en los datos korea del sur el dia 8, esta en los originales
 # rec5[ rec5$Fecha > ymd( 20200305) , c("Fecha", "korea,_south")]
@@ -100,3 +116,32 @@ xts::xts(x = datosAll[ , names(datosAll)[grep( pattern = "italy", x = names(dato
 # x[ grepl(pattern = "orea", `Country/Region` )]
 # x <- fread(input = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
 # x[ grepl(pattern = "orea", `Country/Region` )]
+
+# https://www.elconfidencial.com/multimedia/video/mundo/2020-03-07/como-se-hace-una-cuarentena-masiva-wuhan-la-mayor-cuarentena-de-la-historia_2486287/
+# 20200128
+# https://www.dw.com/es/coronavirus-italia-el-primer-d%C3%ADa-de-la-cuarentena/a-52701099
+# 20200303
+# España
+# 20200114
+plotActivos <- 
+  function(counTxt){
+    x <- data.frame( 
+      Activos = c (0,cumsum( datosAll[ , paste0(counTxt,"_newCase")] ) 
+                   - cumsum( datosAll[ , paste0(counTxt,"_newRecover")] ) 
+                   - cumsum(datosAll[ , paste0(counTxt,"_death")] ) )
+      , recuperados = c(0, cumsum( datosAll[ , paste0(counTxt,"_newRecover")] ) )
+      , muertos = c(0, cumsum( datosAll[ , paste0(counTxt,"_death")] ) )
+      , Totales = c (0,cumsum( datosAll[ , paste0(counTxt,"_newCase")] ) )
+    )
+    x %>% tail(1) ->.;  (.$muertos / (.$muertos + .$recuperados) * 100) %>%  
+      round(2) %>% cat("Ratio Mortalidad sobre muertos y recuperados", . ,"\n")
+    x %>% tail(1) ->.;  (.$muertos / (.$Totales) * 100) %>%  
+      round(2) %>% cat( "Ratio Mortalidad sobre totales", . ,"\n")
+    x$Totales <- rm()
+    zoo( x =  x, order.by = datos$Fecha ) %>% dygraph(main = counTxt)
+}
+
+plotActivos("china")
+plotActivos("korea._south")
+plotActivos("italy")
+plotActivos("spain")
